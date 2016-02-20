63aff5b9a4c6e70fd42bac645b90a420012af8ce
dupontbertrand <dupontbertrand59@gmail.com>
2016-02-20 14:48:39 +0100

    add input field for more tasks

--- todolist.js
+++ todolist.js
@@ -1,21 +1,59 @@
 
 
+Tasks = new Mongo.Collection("tasks");
+
+
+
 if (Meteor.isClient) {
 
   // This code only runs on the client
 
   Template.body.helpers({
 
-    tasks: [
+    tasks: function () {
 
-      { text: "This is task 1" },
+      return Tasks.find({}, {sort: {createdAt: -1}});
 
-      { text: "This is task 2" },
-
-      { text: "This is task 3" }
-
-    ]
+    }
 
   });
+
+
+
+   Template.body.events({
+
+     "submit .new-task": function (event) {
+
+       // Prevent default browser form submit
+
+       event.preventDefault();
+
+
+
+       // Get value from form element
+
+       var text = event.target.text.value;
+
+
+
+       // Insert a task into the collection
+
+       Tasks.insert({
+
+         text: text,
+
+         createdAt: new Date() // current time
+
+       });
+
+
+
+       // Clear form
+
+       event.target.text.value = "";
+
+     }
+
+   });
 
 }
